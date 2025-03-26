661d98a65771e98fe660d9cf424abf8a30043e9f
ecuadros <ecuadros@spc.org.pe>
2025-03-26 09:24:49 -0500

    Main arreglado

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream> // cout
+using namespace std; // Para evitar el std::
 
+// clang++ -o main main.cpp
 int main(){
-    std::cout << "Hola mundo desde Fund Prog 2025-II" << std::endl;
+    cout << "Hola mundo desde Fund Prog 2025-II" << endl;
     return 0;
 }
