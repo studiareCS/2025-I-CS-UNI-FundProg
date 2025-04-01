141c8df261eaa8522761e59d3777138b77fb2f46
ecuadros <ecuadros@spc.org.pe>
2025-04-01 14:37:45 -0500

    #precompiler commands added

--- arit.h
+++ arit.h
@@ -5,3 +5,5 @@
 // Igual a la primera linea de una funcion
 int suma(int a, int b);
 int resta(int a, int b);
+
+#endif
--- main.cpp
+++ main.cpp
@@ -5,13 +5,16 @@ using namespace std; // Para evitar el std::
 // clang++ -o main main.cpp arit.cpp
 // para correr ejecutar: ./main
 
-int main(){
-    cout << "Hola mundo desde Fund Prog 2025-I" << endl;
+void DemoFunciones(){
     cout << suma(54, 35) << endl;
     int x = 65, y = 18;
     int rpta = suma(x, y);
     cout << "La respuesta es: " << rpta << endl;
-
     cout << "La resta es: " << resta(x, y) << endl;
+}
+
+int main(){
+    cout << "Hola mundo desde Fund Prog 2025-I" << endl;
+    DemoFunciones();
     return 0;
 }
