8932d4a8c584d7df2da1091b8c5928fee1916759
studiareCS <yamil.aucca.q@gmail.com>
2025-04-01 13:51:31 -0500

    Main modificado

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@ using namespace std; // Para evitar el std::
 // clang++ -o main main.cpp arit.cpp
 // para correr ejecutar: ./main
 int main(){
-    cout << "Hola mundo desde Fund Prog 2025-II" << endl;
-    cout << suma(5, 10) << endl;
-    int x = 6, y = 8;
+    cout << "Hola mundo desde Fund Prog 2025-I" << endl;
+    cout << suma(54, 35) << endl;
+    int x = 6, y = 98;
     int rpta = suma(x, y);
     cout << "La respuesta es: " << rpta << endl;
     return 0;
