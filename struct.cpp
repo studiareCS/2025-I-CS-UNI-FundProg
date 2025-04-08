ceedf5784da6c032f39211519f7c57de8cb1f3ee
ecuadros <ecuadros@spc.org.pe>
2025-04-08 15:13:13 -0500

    Structs working

--- struct.cpp
+++ struct.cpp
@@ -1,30 +1,65 @@
-#include <iostream>
+
 #include "struct.h"
 
-using namespace std;
-struct Student{
-    string m_name;
-    int    m_age; // Years
-};
-
-void PrintStudent1(Student &s){
+void PrintStudentX(Student &s){
     cout << "Nombre: " << s.m_name 
          << ", Edad: " << s.m_age << endl;
 }
 
-void PrintStudent2(Student *pStudent){
-    cout << "Nombre: " << pStudent->m_name << 
-            ", Edad: " << pStudent->m_age << endl;
+void PrintStudentX(Student *pStudent){
+    PrintStudentX(*pStudent);
+}
+
+void PrintStudentY(Student &s){
+    PrintStudentX(&s);
+}
+
+void PrintStudentY(Student *pStudent){
+    cout << "Nombre: " << pStudent->m_name 
+         << ", Edad: " << pStudent->m_age << endl;
 }
 
 void DemoStructs(){
     Student s1;
     s1.m_name = "Juan";
     s1.m_age  = 20;
-    PrintStudent1(s1);
-
+    PrintStudentX(s1);
+    PrintStudentY(s1);
+    PrintStudentY(&s1);
+    
     Student *p1 = nullptr;
     p1 = &s1;
     p1->m_name = "Gabriela";
-    PrintStudent2(p1);
+    PrintStudentX(p1);
+    PrintStudentX(*p1);
+
+    cout << "Ahora con un objeto dinámico" << endl;
+    Student *p2 = nullptr;
+    p2 = new Student;
+    p2->m_name = "Alfonso";
+    p2->m_age  = 30;
+    PrintStudentX(p2);
+    PrintStudentX(*p2);
+    PrintStudentY(p2);
+    PrintStudentY(*p2);
+    delete p2;
+
+    cout << "Vectores de struct dinamicas" <<endl;
+    Student *pVS = nullptr;
+    pVS = new Student[3];
+    pVS[0].m_name = "Jorge";
+    pVS[0].m_age  = 50;
+    pVS[1].m_name = "Alex";
+    pVS[1].m_age  = 40;
+
+    pVS[2].m_name = "Jaime";
+    (pVS+2)->m_name = "Jaime";
+    (*(pVS+2)).m_name = "Jaime";
+    (*(2+pVS)).m_name = "Jaime";
+    2[pVS].m_name = "Jaime";
+    
+    pVS[2].m_age  = 25;
+    for(int i = 0 ; i < 3 ; ++i)
+        PrintStudentX(pVS[i]);
+    delete [] pVS;
 }
--- struct.h
+++ struct.h
@@ -1,5 +1,19 @@
 #ifndef __STRUCT_H__
 #define __STRUCT_H__
+#include <iostream>
+using namespace std;
+
+struct Student{
+    string m_name;
+    int    m_age; // Years
+};
 
 void DemoStructs();
+
+void PrintStudentX(Student &s);
+void PrintStudentX(Student *pStudent);
+
+void PrintStudentY(Student &s);
+void PrintStudentY(Student *pStudent);
+
 #endif // __STRUCT_H__
